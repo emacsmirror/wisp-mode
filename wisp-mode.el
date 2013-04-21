4aa50a55d6a04bbdcd48a707a282881ed17d829e
Arne Babenhauserheide <bab@draketo.de>
2013-04-21 02:46:03 +0200

    added the skelleton for wisp-mode

---
+++ wisp-mode.el
@@ -0,0 +1,28 @@
+;;; wisp-mode.el --- Major mode for editing wisp: Whitespace-to-Lisp
+
+;; Copyright (C) 2013  Arne Babenhauserheide <arne_bab@web.de>
+
+;; Author: Arne Babenhauserheide <arne_bab@web.de>
+;; Version: 0.0
+;; Keywords: languages, lisp
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'wisp-mode)
+;;; wisp-mode.el ends here
