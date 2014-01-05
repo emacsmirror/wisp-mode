f632d550ac23a0316f9eee55794bb19c57211447
Unknown <drak@kaverne>
2014-01-05 11:19:45 +0100

    bootstrap: fixed success check

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 
 # Bootstrap wisp-guile with wisp.py
 
-diff=$(python3 wisp.py wisp-guile.w > 1 && guile 1 wisp-guile.w > 2 && guile 2 wisp-guile.w > wisp.scm && diff 2 wisp.scm)
-if [[ x"${diff}" == x ]]; then
+diff=$(python3 wisp.py wisp-guile.w > 1 && guile 1 wisp-guile.w > 2 && guile 2 wisp-guile.w > wisp.scm && diff 2 wisp.scm && echo success)
+if [[ x"${diff}" == x"success" ]]; then
     echo "successfully bootstrapped wisp.scm"
 else
     echo "failed to bootstrap wisp.scm. diff: " ${diff}
