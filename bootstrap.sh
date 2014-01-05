2d86cf69c5e898dd67172be059691ebc0b25297c
Unknown <drak@kaverne>
2014-01-05 11:26:27 +0100

    only prepare wisp-at-repl if bootstrapping works.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,11 @@
 diff=$(python3 wisp.py wisp-guile.w > 1 && guile 1 wisp-guile.w > 2 && guile 2 wisp-guile.w > wisp.scm && diff 2 wisp.scm && echo success)
 if [[ x"${diff}" == x"success" ]]; then
     echo "successfully bootstrapped wisp.scm"
+    echo preparing the reader: wisp at the REPL
+    mkdir -p language/wisp
+    guile wisp.scm wisp-reader.w 2>/dev/null > language/wisp/spec.scm \
+        && echo ...succeeded \
+        && echo 'to use wisp at the REPL, run `guile -L` . and then in guile `,L wisp`'
 else
     echo "failed to bootstrap wisp.scm. diff: " ${diff}
 fi
-
-echo preparing the reader: wisp at the REPL
-
-mkdir -p language/wisp
-guile wisp.scm wisp-reader.w 2>/dev/null > language/wisp/spec.scm \
-  && echo ...succeeded \
-  && echo 'to use wisp at the REPL, run `guile -L` . and then in guile `,L wisp`'
