86f796c8139da665884ea8a13ecbd19f24cc9bf1
Arne Babenhauserheide <bab@draketo.de>
2014-01-08 00:15:44 +0100

    Integrated bootstrapping with autotools.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 # Bootstrap wisp-guile with wisp.py
+if [[ x"$1" == x"" ]]; then 
+    srcdir=.
+else
+    srcdir="$1"
+fi
 
-diff=$(python3 wisp.py wisp-guile.w > 1 && guile 1 wisp-guile.w > 2 && guile 2 wisp-guile.w > wisp.scm && diff 2 wisp.scm && echo success)
+diff=$(python3 ${srcdir}/wisp.py ${srcdir}/wisp-guile.w > 1 && guile 1 ${srcdir}/wisp-guile.w > 2 && guile 2 ${srcdir}/wisp-guile.w > wisp.scm && diff 2 wisp.scm && echo success)
 if [[ x"${diff}" == x"success" ]]; then
     echo "successfully bootstrapped wisp.scm"
     echo preparing the reader: wisp at the REPL
     mkdir -p language/wisp
-    guile wisp.scm wisp-reader.w 2>/dev/null > language/wisp/spec.scm \
+    guile wisp.scm ${srcdir}/wisp-reader.w 2>/dev/null > language/wisp/spec.scm \
         && echo ...succeeded \
         && echo 'to use wisp at the REPL, run `guile -L` . and then in guile `,L wisp`'
 else
