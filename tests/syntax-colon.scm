e67860357a9c913c479787f6d4232d540941f7fa
Arne Babenhauserheide <bab@draketo.de>
2014-01-21 18:28:53 +0100

    added explicit test for colon-syntax

---
+++ tests/syntax-colon.scm
@@ -0,0 +1,24 @@
+(let
+  (
+    (a 1)
+    (b 2))
+  (let
+    (
+      (
+        c 3))
+    (format #t "a: ~A, b: ~A, c: ~A"
+                 a      b      c)))
+
+((a))
+
+(define (hello)
+  (display "hello"))
+
+(let
+  ((a 1)
+    (b 2))
+  (format #t "a: ~A, b: ~A"
+               a      b))
+
+
+
