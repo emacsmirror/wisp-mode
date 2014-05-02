abaf560ed27ddc11d247b362fd9ee4855df4bf97
Arne Babenhauserheide <arne@draketo.de>
2014-05-02 23:01:31 +0200

    fix: font-lock-default-face does not exist by default.

--- wisp-mode.el
+++ wisp-mode.el
@@ -75,9 +75,12 @@
      ("\\`#!.*" . font-lock-comment-face) ; initial hashbang
      ("\"\\.\\*\\?" . font-lock-string-face) ; strings (anything between "")
      ; ("^_+ *$" . font-lock-default-face) ; line with only underscores
-                                         ; and whitespace shown as
-                                         ; default text. This is just
-                                         ; a bad workaround. Which does not work.
+                                           ; and whitespace shown as
+                                           ; default text. This is just
+                                           ; a bad workaround. 
+                                           ; Which does not work because 
+                                           ; *-default-face is not guaranteed 
+                                           ; to be defined.
      ("^\\(?:_* +\\| *\\): *$" . font-lock-keyword-face) ; line with only a : + whitespace, not at the beginning
      ("^\\(?:_* +\\| *\\): \\| *\\. " . font-lock-keyword-face) ; leading : or .
      ( ,(regexp-opt wisp-builtin 'symbols) . font-lock-builtin-face) ; generic functions
