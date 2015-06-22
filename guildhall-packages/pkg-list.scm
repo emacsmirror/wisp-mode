46c0f2d80a61be889fa03ba74edc6163e05e3c00
Arne Babenhauserheide <bab@draketo.de>
2015-06-22 23:47:12 +0200

    newbase60 guildhall package: does not need depends (all deps are in guile).

--- guildhall-packages/pkg-list.scm
+++ guildhall-packages/pkg-list.scm
@@ -1,6 +1,5 @@
 (package (newbase60 (0))
          (synopsis "Implementation of Tanteks New Base 60")
-         (depends (srfi) (ice-9))
          (libraries
           (scm -> "newbase60"))
          (programs
