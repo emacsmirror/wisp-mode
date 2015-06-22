25c63407342339374a25d013ab42a04c486cd9de
Arne Babenhauserheide <bab@draketo.de>
2015-06-22 13:24:46 +0200

    autoload the hook

--- wisp-mode.el
+++ wisp-mode.el
@@ -49,6 +49,7 @@
 ; use this mode automatically
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.w\\'" . wisp-mode))
+;;;###autoload
 (add-hook 'wisp-mode-hook
           (lambda ()
             (electric-indent-local-mode -1)))
