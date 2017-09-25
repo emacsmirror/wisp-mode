eb8d4af44ccd935568be192f64317b3dedb7d3d9
Arne Babenhauserheide <bab@draketo.de>
2017-09-25 21:54:22 +0200

    expand test for double-reduction of indentation levels

--- tests/partial-indent.scm
+++ tests/partial-indent.scm
@@ -1,6 +1,13 @@
-(write 
+(write
  (list
     (+ 1 2)
+   (+ 2 3)))
+(newline)
+
+(write
+ (list
+    (+ 1 2
+      (+ 3 4))
    (+ 2 3)))
 (newline)
 
--- tests/partial-indent.w
+++ tests/partial-indent.w
@@ -1,5 +1,12 @@
-write 
+write
  list
     + 1 2
+   + 2 3
+newline
+
+write
+ list
+    + 1 2
+      + 3 4
    + 2 3
 newline
